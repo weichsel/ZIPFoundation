54cab7361868c2f4b7aec215ad06326b0cd5aa3d
Thomas Zoechling <thomas@peakstep.com>
2019-04-04 10:42:38 +0200

    Add systemLibrary target and accompanying module map

--- Package.swift
+++ Package.swift
@@ -2,32 +2,25 @@
 import PackageDescription
 
 #if canImport(Compression)
-let dependencies: [Package.Dependency] = []
+let targets: [Target] = [
+    .target(name: "ZIPFoundation"),
+    .testTarget(name: "ZIPFoundationTests", dependencies: ["ZIPFoundation"])
+]
 #else
-let dependencies: [Package.Dependency] = [
-    .package(url: "https://github.com/IBM-Swift/CZlib.git", .exact("0.1.2"))
+let targets: [Target] = [
+    .systemLibrary(name: "CZLib", pkgConfig: "zlib", providers: [.brew(["zlib"]), .apt(["zlib"])]),
+    .target(name: "ZIPFoundation", dependencies: ["CZLib"]),
+    .testTarget(name: "ZIPFoundationTests", dependencies: ["ZIPFoundation"])
 ]
 #endif
 
 let package = Package(
-    name: "ZIPFoundation",
-    platforms: [
-        .macOS(.v10_11)
-    ],
-    products: [
-        .library(
-            name: "ZIPFoundation",
-            targets: ["ZIPFoundation"]
-        )
-    ],
-    dependencies: dependencies,
-    targets: [
-        .target(
-            name: "ZIPFoundation"
-        ),
-        .testTarget(
-            name: "ZIPFoundationTests",
-            dependencies: ["ZIPFoundation"]
-        )
-    ]
+name: "ZIPFoundation",
+platforms: [
+    .macOS(.v10_11)
+],
+products: [
+    .library(name: "ZIPFoundation", targets: ["ZIPFoundation"])
+],
+targets: targets
 )
---
+++ Sources/CZLib/shim.h
@@ -0,0 +1,17 @@
+//
+//  shim.h
+//  ZIPFoundation
+//
+//  Copyright © 2017-2019 Thomas Zoechling, https://www.peakstep.com and the ZIP Foundation project authors.
+//  Released under the MIT License.
+//
+//  See https://github.com/weichsel/ZIPFoundation/blob/master/LICENSE for license information.
+//
+
+#ifndef zlib_shim_h 
+#define zlib_shim_h
+
+#import <stdio.h>
+#import <zlib.h>
+
+#endif
