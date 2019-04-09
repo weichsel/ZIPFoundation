2cb61ac0543bca930f5d0030c57a63aa60dd367a
Thomas Zoechling <thomas@peakstep.com>
2019-04-09 17:29:11 +0200

    Add platform versions for Darwin platforms

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let targets: [Target] = [
 let package = Package(
 name: "ZIPFoundation",
 platforms: [
-    .macOS(.v10_11)
+    .macOS(.v10_11), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
 ],
 products: [
     .library(name: "ZIPFoundation", targets: ["ZIPFoundation"])
