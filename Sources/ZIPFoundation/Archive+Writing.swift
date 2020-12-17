20102f58d4914a46aeb9b4614db540a679aa62aa
brice <briceguard-git@yahoo.com>
2020-12-17 15:07:24 +0000

    Provide addEntry api to enable name in the archive to be different from the file name

--- Sources/ZIPFoundation/Archive+Writing.swift
+++ Sources/ZIPFoundation/Archive+Writing.swift
@@ -29,8 +29,25 @@ extension Archive {
     /// - Throws: An error if the source file cannot be read or the receiver is not writable.
     public func addEntry(with path: String, relativeTo baseURL: URL, compressionMethod: CompressionMethod = .none,
                          bufferSize: UInt32 = defaultWriteChunkSize, progress: Progress? = nil) throws {
+        let fileURL = baseURL.appendingPathComponent(path)
+
+        try self.addEntry(with: path, fileURL: fileURL, compressionMethod: compressionMethod, bufferSize: bufferSize, progress: progress)
+    }
+    
+    /// Write files, directories or symlinks to the receiver.
+    ///
+    /// - Parameters:
+    ///   - path: The path that is used to identify an `Entry` within the `Archive` file.
+    ///   - fileURL: The file URL with the full path of the `Entry` to add.
+    ///   - compressionMethod: Indicates the `CompressionMethod` that should be applied to `Entry`.
+    ///                        By default, no compression will be applied.
+    ///   - bufferSize: The maximum size of the write buffer and the compression buffer (if needed).
+    ///   - progress: A progress object that can be used to track or cancel the add operation.
+    /// - Throws: An error if the source file cannot be read or the receiver is not writable.
+    public func addEntry(with path: String, fileURL : URL, compressionMethod: CompressionMethod = .none,
+                         bufferSize: UInt32 = defaultWriteChunkSize, progress: Progress? = nil) throws {
         let fileManager = FileManager()
-        let entryURL = baseURL.appendingPathComponent(path)
+        let entryURL = fileURL
         guard fileManager.itemExists(at: entryURL) else {
             throw CocoaError(.fileReadNoSuchFile, userInfo: [NSFilePathErrorKey: entryURL.path])
         }
