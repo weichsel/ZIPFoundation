ec2a0d6cbb749816c84f3ae7d56faee5cc0cdb2e
Thomas Zoechling <thomas@peakstep.com>
2026-05-15 20:09:12 +0200

    Rename shim

--- Sources/ZIPFoundation/Shims+Windows.swift
+++ Sources/ZIPFoundation/Shims+Windows.swift
@@ -1,5 +1,5 @@
 //
-//  PosixCompat+Windows.swift
+//  Shims+Windows.swift
 //  ZIPFoundation
 //
 //  Copyright © 2017-2026 Thomas Zoechling, https://www.peakstep.com and the ZIP Foundation project authors.
